54cc18b5b680710be132d8996d1c927591aee5d2
Vince Santos <vincecarlofsantos@gmail.com>
2022-06-09 23:50:15 +0800

    as framework

---
+++ SimplyChainway/SimplyChainway.h
@@ -0,0 +1,19 @@
+//
+//  SimplyChainway.h
+//  SimplyChainway
+//
+//  Created by Vince Carlo Santos on 6/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimplyChainway.
+FOUNDATION_EXPORT double SimplyChainwayVersionNumber;
+
+//! Project version string for SimplyChainway.
+FOUNDATION_EXPORT const unsigned char SimplyChainwayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplyChainway/PublicHeader.h>#import "RFIDBlutoothManager.h"
+#import "RFIDBlutoothManager.h"
+#import "BLEModel.h"
+#import "BluetoothUtil.h"
